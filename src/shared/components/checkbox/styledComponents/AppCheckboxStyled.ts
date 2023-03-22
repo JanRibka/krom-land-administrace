70767770759fee62a63a6e3dc67dc12eee375bbc
JanRibka <62721322+JanRibka@users.noreply.github.com>
2023-03-22 23:20:02 +0100

    Poznámka k ceně

--- src/shared/components/checkbox/styledComponents/AppCheckboxStyled.ts
+++ src/shared/components/checkbox/styledComponents/AppCheckboxStyled.ts
@@ -1,9 +1,10 @@
-import FormControlLabel from "@mui/material/FormControlLabel";
-import { styled } from "@mui/material/styles";
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { styled } from '@mui/material/styles';
 
 const AppCheckboxStyled = styled(FormControlLabel)(({ theme }) => ({
   svg: {
     fontSize: "x-large",
+    color: theme.palette.text.secondary,
   },
 }));
 
