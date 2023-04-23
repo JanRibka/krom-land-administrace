fccbdcd50b667bb4d4f71afdec0c6a7f8da1aa5f
JanRibka <62721322+JanRibka@users.noreply.github.com>
2023-04-23 21:44:59 +0200

    Zobrazení devtools podle prostředí

--- src/shared/infrastructure/store/store.ts
+++ src/shared/infrastructure/store/store.ts
@@ -1,9 +1,9 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
-import AppState from './AppState';
-import authenticationReduce from './authentication/authenticationSlice';
-import commonReduce from './common/commonSlice';
-import webPartsReduce from './webParts/webPartsSlice';
+import AppState from "./AppState";
+import authenticationReduce from "./authentication/authenticationSlice";
+import commonReduce from "./common/commonSlice";
+import webPartsReduce from "./webParts/webPartsSlice";
 
 const rootReducer = combineReducers<AppState>({
   webParts: webPartsReduce,
@@ -16,5 +16,5 @@ const rootReducer = combineReducers<AppState>({
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-  devTools: false,
+  devTools: process.env.REACT_APP_ENABLE_DEVTOOLS === "true",
 });
