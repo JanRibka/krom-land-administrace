84d7b82daef3648d089ca1cb1645de09aef33e29
JanRibka <62721322+JanRibka@users.noreply.github.com>
2023-05-01 08:21:44 +0200

    WebContent

--- api/controllers/WebContentController.php
+++ api/controllers/WebContentController.php
@@ -1,32 +1,31 @@
 <?php
+
 namespace komLand\api\controllers;
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: GET, POST");
-header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Content-Type: application/json');
 
+require_once __DIR__.'/./ControllerBase.php';
+require_once __DIR__.'/../enums/httpStatucCode.php';
+require_once __DIR__.'/../enums/UserRoleEnum.php';
+require_once __DIR__.'/../middleware/verifyJWT.php';
+require_once __DIR__.'/../middleware/verifyRole.php';
+require_once __DIR__.'/../repositories/WebContentRepository.php';
+require_once __DIR__.'/../services/WebContentService.php';
 
-require_once __DIR__ . "/./ControllerBase.php";
-require_once __DIR__ . "/../enums/httpStatucCode.php";
-require_once __DIR__ . "/../enums/UserRoleEnum.php";
-require_once __DIR__ . "/../middleware/verifyJWT.php";
-require_once __DIR__ . "/../middleware/verifyRole.php";
-require_once __DIR__ . "/../repositories/WebContentRepository.php";
-require_once __DIR__ . "/../services/WebContentService.php";
-
-use function kromLand\api\middleware\verifyJWT;
-use function kromLand\api\middleware\verifyRole;
-
-use Exception;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
 use kromLand\api\controllers\ControllerBase;
 use kromLand\api\enums\HttpStatusCode;
 use kromLand\api\enums\UserRoleEnum;
-use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\ServerRequest;
 use kromLand\api\repositories\WebContentRepository;
-use kromLand\api\services\WebContentService;
 use kromLand\api\services\IWebContentService;
+use kromLand\api\services\WebContentService;
+
+use function kromLand\api\middleware\verifyJWT;
+use function kromLand\api\middleware\verifyRole;
 
 class WebContentController extends ControllerBase
 {
@@ -37,66 +36,93 @@ class WebContentController extends ControllerBase
         $this->_homeService = $pHomeService;
     }
 
+    /**
+     * Get Home data.
+     */
     public function getHome()
     {
-        try
-        {
+        try {
             $home = $this->_homeService->getHome(1);
 
-            $this->apiResponse(true, "", $home);
-        } catch(Exception $ex)
-        {
+            $this->apiResponse(true, '', $home);
+        } catch (\Exception $ex) {
+            $this->apiResponse(false, $ex->getMessage(), null, HttpStatusCode::INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Get Actions data.
+     */
+    public function getActions()
+    {
+        try {
+            $home = $this->_homeService->getHome(1);
+
+            $this->apiResponse(true, '', $home);
+        } catch (\Exception $ex) {
             $this->apiResponse(false, $ex->getMessage(), null, HttpStatusCode::INTERNAL_SERVER_ERROR);
         }
     }
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') { 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (isset($_GET['function'])) {
-        $userRoles = [
-            "getHome" => [
-                UserRoleEnum::ADMIN
-            ],
-        ];        
+        $functionName = $_GET['function'];
 
-        $functionName = $_GET['function'];        
+        $userRoles = [UserRoleEnum::ADMIN];
+
+        switch ($functionName) {
+            case 'getHome':
+            case 'getActions':
+            case 'getGallery':
+            case 'getContact':
+            case 'getGdpr':
+            case 'getTermOfConditions':
+                $userRoles = [
+                    UserRoleEnum::ADMIN,
+                    UserRoleEnum::EDITOR,
+                    UserRoleEnum::USER,
+                ];
+
+                break;
+        }
+
         $webContentRepository = new WebContentRepository();
         $webContentService = new WebContentService($webContentRepository);
-        $controller = new WebContentController($webContentService);         
-        
+        $controller = new WebContentController($webContentService);
+
         if (method_exists($controller, $functionName)) {
             $request = new ServerRequest(
                 $_SERVER['REQUEST_METHOD'],
                 $_SERVER['REQUEST_URI'],
                 $_SERVER
-            );            
-            $response = new Response();       
-            $response = verifyJWT($request, $response, 
-                function($request, $response) use ($controller, $functionName, $userRoles) {
+            );
+            $response = new Response();
+            $response = verifyJWT($request, $response,
+                function ($request, $response) use ($controller, $functionName, $userRoles) {
                     return verifyRole($userRoles[$functionName])($request, $response,
-                    function($request, $response) use($controller, $functionName){
-                        call_user_func([$controller, $functionName]);
-                            
-                        return $response;
-                    }, false);
-                }, false); 
+                        function ($request, $response) use ($controller, $functionName) {
+                            call_user_func([$controller, $functionName]);
+
+                            return $response;
+                        }, false);
+                }, false);
 
             $statusCode = $response->getStatusCode();
 
             if ($statusCode !== HttpStatusCode::OK) {
                 http_response_code($response->getStatusCode());
                 echo json_encode([
-                    "Success" => false,
-                    "ErrMsg" => "Pro provedení akce nemáte dostatečná oprávnění"        
-                ], JSON_UNESCAPED_UNICODE);            
-            }                    
+                    'Success' => false,
+                    'ErrMsg' => 'Pro provedení akce nemáte dostatečná oprávnění',
+                ], JSON_UNESCAPED_UNICODE);
+            }
         } else {
             http_response_code(HttpStatusCode::INTERNAL_SERVER_ERROR);
             echo json_encode([
-                "Success" => false,
-                "ErrMsg" => "Chybná funkce, nebo nebyla zadána"                
-            ], JSON_UNESCAPED_UNICODE);            
-        } 
-    } 
+                'Success' => false,
+                'ErrMsg' => 'Chybná funkce, nebo nebyla zadána',
+            ], JSON_UNESCAPED_UNICODE);
+        }
+    }
 }
-?>
