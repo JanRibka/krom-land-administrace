d08beec415d470bfeb5f29aabcb5d1097e42c601
JanRibka <62721322+JanRibka@users.noreply.github.com>
2023-05-05 12:03:00 +0200

    Ukládání obrázků

--- api/repositories/IImageRepository.php
+++ api/repositories/IImageRepository.php
@@ -4,9 +4,23 @@ namespace kromLand\api\repositories;
 
 interface IImageRepository
 {
-    public function imageInsert(string $image, string $imageName, string $description): int;
+    public function imageInsertGalleryImage(string $image, string $imageName, string $description): int;
 
     public function imageUpdate(string $name, string $description, int $id): void;
 
-    public function imageSaveHome(string $image, string $imageName, int $homeId): void;
+    public function imageUpdateHome(string $image, string $imageName, int $homeId): void;
+
+    public function imageUpdateTeamMembers(string $image, string $imageName, int $homeId): void;
+
+    public function imageUpdateActions(string $image, string $imageName, int $homeId): void;
+
+    public function imageUpdateActionDetails(string $image, string $imageName, int $homeId): void;
+
+    public function imageUpdateGallery(string $image, string $imageName, int $homeId): void;
+
+    public function imageUpdateGalleryImage(string $image, string $imageName, int $homeId): void;
+
+    public function imageUpdateContact(string $image, string $imageName, int $homeId): void;
+
+    public function imageDeleteGalleryImage(int $id): void;
 }
