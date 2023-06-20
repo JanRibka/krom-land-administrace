1d4449bb7f99ff37ceeb6c2ee5677a9822184f2c
JanRibka <62721322+JanRibka@users.noreply.github.com>
2023-06-20 23:19:24 +0200

    Title na tlačítkách v menu

--- src/features/layout/sideBar/navLinks/NavLinksDashboard.tsx
+++ src/features/layout/sideBar/navLinks/NavLinksDashboard.tsx
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
-import Box from "@mui/material/Box";
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import Box from '@mui/material/Box';
 
-import { AppRoute } from "../../../../shared/infrastructure/router/appRoutes";
-import NavLinksStyled from "./styledComponents/NavLinksStyled";
+import { AppRoute } from '../../../../shared/infrastructure/router/appRoutes';
+import NavLinksStyled from './styledComponents/NavLinksStyled';
 
 const NavLinksDashboard = () => {
   return (
     <NavLinksStyled component='ul'>
       {/* Dashboard */}
-      <Box component='li' data-route={AppRoute.Dashboard}>
+      <Box component='li' data-route={AppRoute.Dashboard} title='Dashboard'>
         <Link to={AppRoute.Dashboard}>
           {
             <Box className='link-inner-wrapper'>
